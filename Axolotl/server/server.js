01282576f09af26c0c55e0d9c3fef4d6d7399d76
Peter Makhnatch <p.makhnatch@gmail.com>
2022-10-01 14:32:24 -0400

    10/1 connected to the database

--- Axolotl/server/server.js
+++ Axolotl/server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const db = require('../models/database');
+
 
 const app = express();
 app.use(cors());
@@ -9,6 +11,15 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 console.log("hello world");
+
+const testFunction = async () => {
+  const add = `INSERT INTO users (name, email, password, location) VALUES ('Yirou', 'yirou@gmail.com', '6789', 'new york city')`
+  await db.query(add)
+  console.log('textFunction executed')
+}
+testFunction();
+
+
 
 
 
