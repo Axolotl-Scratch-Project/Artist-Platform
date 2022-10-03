b6c3b907b2046944f7042a94e173a84c01780c93
hxdr <raymondhuang2015@gmail.com>
2022-10-03 14:04:01 -0700

    updatemainpage

--- Axolotl/client/pages/Home.jsx
+++ Axolotl/client/pages/Home.jsx
@@ -1,7 +1,29 @@
+import React from 'react'
+import Feed from '../component/Feed'
+import Navbar from '../component/Navbar'
+import SideBar from '../component/SideBar'
+import SideBar2 from '../component/SideBar2'
+
+import { Box , Stack,Grid } from '@mui/material';
+import { Category, PriceChange } from '@mui/icons-material';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+      <Box>
+        <Navbar/>
+        {/* <Stack direction='row' justifyContent='space-between'>
+          <SideBar/>
+          <Feed/>
+        </Stack> */}
+        <Grid container spacing={2} columns={10}>
+          <Grid item xs={4}>
+          <SideBar2/>
+            </Grid>
+          <Grid item xs={6}>
+          <Feed/>
+            </Grid>
+          </Grid>
+      </Box>
   )
 }
 
