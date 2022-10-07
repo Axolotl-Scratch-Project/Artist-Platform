6eb0ed6a9cfaf7629ec1fc42c847475b86e173e9
Yirou <yirou.zm@gmail.com>
2022-10-07 15:26:13 -0400

    booking page and booking table done

--- Axolotl/client/pages/Booking.jsx
+++ Axolotl/client/pages/Booking.jsx
@@ -5,57 +5,84 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { Navigate } from "react-router-dom";
 import PersonAddAlt1OutlinedIcon from '@mui/icons-material/PersonAddAlt1Outlined';
 import axios from 'axios';
+import UserViewNav from '../component/UserViewNav'
+import BookingTable from '../component/BookingTable'
+
 
 const Booking = () => {
 
-  const paperStyle = {padding: '30px 20px', width: 'calc(85% - 5px)', margin: '20px auto', borderradius: '10px'};
+  const paperStyle = {padding: '25px 20px', width: 'calc(95% - 5px)', margin: '20px auto', borderradius: '10px'};
   const boxStyle = {padding: '0px 23px'}
-  const confirmStyle = {color: '#61d800', display: 'inline', justifyContent: 'center', fontSize: '28px', fontWeight: 'bold', margin: '10px'};
+  const confirmStyle = {color: '#4caf50', display: 'inline', justifyContent: 'center', fontSize: '28px', fontWeight: 'bold'};
+  const currentStyle = {fontSize: '16px', fontFamily: 'Arial',};
   const iconStyle = { margin:'auto 8px',fontSize: '28px', fontWeight: 'bold'};
+  const tableStyle = {padding: '20px 20px'};
+  const activeStyle = {fontSize: '23px', color: '#4caf50', fontFamily: 'Arial'}
+
 
   //get user info from localStoage
+  // const bookingInfo = {
+  //   bookerId: JSON.parse(window.localStorage.getItem('userId')),
+  //   bookerType: JSON.parse(window.localStorage.getItem('user'))
+  // }
+
+  //formate date- talk to Peter about adding to create booking and get booking controller
+  const formatDate = (dateString) => {
+    const newDate = dateString.slice(0, dateString.length-1) + dateString[dateString.length-1];
+    const options = { year: "numeric", month: "long", day: "numeric" }
+    const time = new Date(dateString).toLocaleTimeString(undefined, options)
+    return time
+  };
+
+
   const bookingInfo = {
-    bookerId: JSON.parse(window.localStorage.getItem('bookerId')),
-    bookerType: JSON.parse(window.localStorage.getItem('bookerType'))
+    bookerId: 1,
+    bookerType: 'user'
   }
 
-  const [booking, setBooking] = useState([]);
+  const [bookings, setBookings] = useState([]);
+  const [latestBooking, setLatestBooking] = useState({});
 
   useEffect(() => {
-    axios.post('/api/booking', bookingInfo).then(res => {
-      setBooking(res.data.bookingsByUser);
+    axios.post('/api/getBooking', bookingInfo).then(res => {
+      // localstorage
+      // create a variable
+      const reformatBooking = res.data.bookingsByUser.map(booking => {
+        booking.booking_start = formatDate(booking.booking_start);
+        booking.booking_end =  formatDate(booking.booking_end);
+        return booking;
+      })
+      // setBookings(res.data.bookingsByUser);
+      setBookings(reformatBooking);
+      setLatestBooking(reformatBooking[reformatBooking.length - 1]);
     })
-  });
+  }, []);
 
-  // const latestBooking = booking[booking.length - 1];
-  // static booking for testing purpose
-  const latestBooking ={
-    "id": 23,
-    "artist_id": 3,
-    "amount": 1200,
-    "booking_start": "2022-10-07T12:27:00.000Z",
-    "booking_end": "2022-10-07T18:27:00.000Z",
-    "booker_id": 1,
-    "booker_type": "user"
-}
+
 
   return (
-    <Grid style = {{margin: '100px auto'}}>
+    <Grid>
+    <UserViewNav />
+    <Grid style = {{margin: '40px auto'}}>
     {/* <Navbar /> */}
     <Paper elevation = {20} style = {paperStyle}>
       <Box style = {boxStyle}>
-        {/* <Avatar style = {avatarStyle}><PersonAddAlt1OutlinedIcon /></Avatar> */}
         <div style = {confirmStyle} >
           <Typography variant="h5">Booking Confirmed  <CheckCircleIcon style = {iconStyle} /></Typography>
         </div>
-        <div>
-        <Typography variant="h6"> {`You are successfully booked with ${latestBooking['artist_id']}`}</Typography>
+        <div style = {currentStyle}>
+        <p variant="h6"> {`Hi ${latestBooking.booker_name}, you are successfully booked with ${latestBooking.artist_name}!`}</p>
+        <p variant="h6"> {` Get excited for your session at ${latestBooking.booking_start}!`}</p>
         </div>
       </Box>
-    </Paper>
-    <Paper elevation = {20} style = {paperStyle}>
-    <h2  >Your Other Bookings</h2>
-    </Paper>
+      <Box style = {tableStyle}>
+        <p style = {activeStyle} variant="h5">Your Active Bookings </p>
+        <BookingTable bookings = {bookings} />
+      </Box>
+      </Paper>
+
+    {/* </Paper> */}
+  </Grid>
   </Grid>
 
   )
@@ -64,3 +91,4 @@ const Booking = () => {
 }
 
 export default Booking;
+
--- Axolotl/client/pages/Signup.jsx
+++ Axolotl/client/pages/Signup.jsx
@@ -36,7 +36,7 @@ const Signup = () => {
         // window.localStorage.setItem('testing', JSON.stringify())
         const artistData = res.data.artistData;
         window.localStorage.setItem('artistId', JSON.stringify(artistData.id));
-        window.localStorage.setItem('userType', JSON.stringify('artist'));
+        window.localStorage.setItem('userType', JSON.stringify("artist"));
         navigate("/signup/redirect");
       })
     }
@@ -45,7 +45,7 @@ const Signup = () => {
         //need to redirect to diff home page based on user type- waiting on Raymond's main page
         const userData = res.data.newUserData;
         window.localStorage.setItem('userId', JSON.stringify(userData.id));
-        window.localStorage.setItem('userType', JSON.stringify('user'));
+        window.localStorage.setItem('userType', JSON.stringify("user"));
         navigate("/signup/redirect");
       })
     }
